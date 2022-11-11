94171f6a6a0200dc86948387f99051e83a24a719
Mr.Shital B.Madake <sbmadake@gmail.com>
2022-11-11 15:54:37 +0530

    first commit

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+class App extends React.Component {
+
+    render() {
+        return (<div>
+            <h1>new changes NEW This is my react application</h1>
+            <h1>Another line</h1>
+            
+            </div>)
+    }
+
+}
+
+export default App
